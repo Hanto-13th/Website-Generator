20cec645891232fc9f89c2d1a4a4d6cfe8a45f13
Hanto_13th <antoninhidien@orange.fr>
2025-11-22 15:38:21 +0100

    Add functions to split and handle differents markdown style into a text

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -64,19 +64,3 @@ class ParentNode(HTMLNode):
     def __repr__(self):
         return f"ParentNode({self.tag}, {self.children}, {self.props})"
     
-
-def text_node_to_html_node(text_node):
-    if text_node.text_type not in TextType:
-        raise Exception("Type used is unknown")
-    if text_node.text_type == TextType.TEXT:
-        return LeafNode(None,text_node.text)
-    if text_node.text_type == TextType.BOLD:
-        return LeafNode("b",text_node.text)
-    if text_node.text_type == TextType.ITALIC:
-        return LeafNode("i",text_node.text)
-    if text_node.text_type == TextType.CODE:
-        return LeafNode("code",text_node.text)
-    if text_node.text_type == TextType.LINK:
-        return LeafNode("a",text_node.text,{"href": f"{text_node.url}"})
-    if text_node.text_type == TextType.IMAGE:
-        return LeafNode("img","",{"src": f"{text_node.url}","alt": f"{text_node.text}"})
